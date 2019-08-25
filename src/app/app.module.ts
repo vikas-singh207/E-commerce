f7ea11ddf419d2a99a7d19ed4f53dc26ccaf6fa3
vikas-singh207 <vikas.mca.du.2014@gmail.com>
2019-08-25 11:34:09 +0530

    7 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {NavbarComponent} from '../components/navbar/navbar.component';
+
+//import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import {HomeComponent} from '../components/home/home.component';
 
-
+/*const adminRoutes: Routes = [
+  {path:'', component:HomeComponent}
+];
+*/
+//,RouterModule.forRoot(adminRoutes)],
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent,NavbarComponent ],
+  declarations: [ AppComponent,NavbarComponent ,HomeComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: [ './navbar.component.scss' ]
 })
-export class NavbarComponent  {
+export class NavbarComponent implements OnInit {
+  
+  constructor(){
+   let userName:string;
+    userName="Vikas";
+  }
+
+  
+  ngOnInit(){}
+
   
 }
