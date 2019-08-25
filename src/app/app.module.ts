251bd09b58683bb50a9a2bea1cf9f1eefd665afc
vikas-singh207 <vikas.mca.du.2014@gmail.com>
2019-08-25 21:46:10 +0530

    7 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {NavbarComponent} from '../components/navbar/navbar.component';
 
-//import { RouterModule, Routes } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import {HomeComponent} from '../components/home/home.component';
+import {ShoppingcartComponent} from '../components/shoppingcart/shoppingcart.component';
+import {LoginComponent} from '../components/login/login.component';
+import {SignupComponent} from '../components/signup/signup.component';
 
-/*const adminRoutes: Routes = [
-  {path:'', component:HomeComponent}
+const adminRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'cart', component: ShoppingcartComponent},
+  {path:'login', component: LoginComponent},
+  {path:'signup', component: SignupComponent}
 ];
-*/
+
 //,RouterModule.forRoot(adminRoutes)],
 @NgModule({
-  imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent,NavbarComponent ,HomeComponent],
+  imports:      [ BrowserModule, FormsModule,NgbModule,RouterModule.forRoot(adminRoutes),FontAwesomeModule],
+  declarations: [ AppComponent,NavbarComponent ,HomeComponent,ShoppingcartComponent,LoginComponent,SignupComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component ,OnInit} from '@angular/core';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,12 +7,14 @@ import { Component ,OnInit} from '@angular/core';
   styleUrls: [ './navbar.component.scss' ]
 })
 export class NavbarComponent implements OnInit {
-  
+  userName:string;
+  cartItemsNumber:number;
   constructor(){
-   let userName:string;
-    userName="Vikas";
+   
+   this.userName="Vikas";
+   this.cartItemsNumber=3;
   }
-
+  faCartPlus = faCartPlus;
   
   ngOnInit(){}
 
